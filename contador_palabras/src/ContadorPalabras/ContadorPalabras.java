99979b52cde27d145904e62c2ab6b1df78b991cd
yesidf28 <yesid_28@hotmail.com>
2024-01-17 21:30:13 -0500

    cambio 3

--- contador_palabras/src/ContadorPalabras/ContadorPalabras.java
+++ contador_palabras/src/ContadorPalabras/ContadorPalabras.java
@@ -13,42 +13,38 @@ public class ContadorPalabras {
         static int palabrasTotales;
 
         public static void main(String[] args) {
-            // VARIABLE PARA LLEVAR EL CONTADOR DE PALABRAS
+
             palabrasTotales = 0;
 
-            // OBJETO ARCHIVO CON LA RUTA DEL ARCHIVO A LEER
+
             File archivo = new File("src/Archivo.txt");
 
-            // LLAMADA AL METODO PARA CONTAR LAS PALABRAS Y LAS LINEAS
+
             contarPalabras(archivo);
         }
 
-        // METODO QUE LEE EL ARCHIVO Y CUENTA LAS PALABRAS Y LAS LINEAS
+
         public static void contarPalabras(File archivo) {
             try {
-                // SI EXISTE EL ARCHIVO
+
                 if(archivo.exists()) {
-                    // ABRE LECTURA
+
                     ArrayList<String> palabras = new ArrayList<String>();
                     HashMap<String,Integer> palabras2 = new HashMap<String,Integer>();
                     BufferedReader archivoLeer = new BufferedReader(new FileReader(archivo));
-                    System.out.println(archivoLeer.read());
+                    
                     String lineaLeida;
 
-                    // MIENTRAS LA LINEA LEIDA NO SEA NULL
+
                     while((lineaLeida = archivoLeer.readLine()) != null) {
                         //System.out.println(lineaLeida);
 
-                        // CONTAMOS LAS PALABRAS DE LA LINEA
+
                         StringTokenizer st = new StringTokenizer(lineaLeida);
                         while (st.hasMoreTokens()) {
                             palabras.add(st.nextToken());
                         }
 
-
-                        // AUMENTA EL CONTADOR DE LINEAS Y DE PALABRAS
-
-                        //palabrasTotales = palabrasTotales + st.countTokens();
                     }
                     Iterator<String> nombreIterator = palabras.iterator();
                     while(nombreIterator.hasNext()){
@@ -62,7 +58,7 @@ public class ContadorPalabras {
                     System.out.println("");
                     System.out.println("Numero de palabras: " + palabras.size());
 
-                    // CIERRA LA LECTURA
+
                     archivoLeer.close();
                 } else {
                     System.out.println("NO EXISTE EL ARCHIVO");
